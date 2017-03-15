0351a8ebc87827408ed109bab079583269b91857
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-15 14:44:54 +0530

    WIP

---
+++ lib/elm-functional-helpers/FunctionalHelpers.elm
@@ -0,0 +1,52 @@
+module FunctionalHelpers exposing (..)
+
+
+(=>) =
+    (,)
+infix 9 =>
+
+
+ifElse : (a -> Bool) -> (a -> b) -> (a -> b) -> a -> b
+ifElse pred onTrue onFalse value =
+    if pred value then
+        onTrue value
+    else
+        onFalse value
+
+
+whenBool bool =
+    when (always bool)
+
+
+when : (a -> Bool) -> (a -> a) -> a -> a
+when pred onTrue value =
+    ifElse pred onTrue (\_ -> value) value
+
+
+unless : (a -> Bool) -> (a -> a) -> a -> a
+unless pred =
+    when (pred >> not)
+
+
+gt =
+    (>)
+
+
+lt =
+    (<)
+
+
+or =
+    (||)
+
+
+and =
+    (&&)
+
+
+equals =
+    (==)
+
+
+notEquals =
+    (/=)

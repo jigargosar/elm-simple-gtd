d0252d4ce77680835af3786d4133d00b87c3e055
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-16 18:53:36 +0530

    WIP

--- src/elm/Flow.elm
+++ src/elm/Flow.elm
@@ -4,38 +4,6 @@ import Toolkit.Operators exposing (..)
 import Toolkit.Helpers exposing (..)
 
 
-type Branch
-    = Leaf
-        { question : String
-        , onBack : Maybe Branch
-        }
-    | YesNoBranch
-        { question : String
-        , onBack : Maybe Branch
-        , onYes : Branch
-        , onNo : Branch
-        }
-
-
-isActionable =
-    YesNoBranch
-        { question = "Is it Actionable?"
-        , onYes = canItBeDoneUnder2Min (\_ -> Just isActionable)
-        , onNo = isItWorthKeeping (\_ -> Just isActionable)
-        , onBack = Nothing
-        }
-        |> Debug.log "isActionable"
-
-
-canItBeDoneUnder2Min back =
-    Leaf
-        { question = "Can it be done in less than 2 minutes ?"
-        , onBack = back ()
-        }
-
-
-isItWorthKeeping back =
-    Leaf
-        { question = "Is it worth keeping ?"
-        , onBack = back ()
-        }
+type Node
+    = Branch String Node Node
+    | Leaf String

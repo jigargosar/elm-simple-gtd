1e776d0979ca207961e51a3c26d7aafdcc601079
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-17 16:04:13 +0530

    WIP

--- src/elm/InBasketFlow.elm
+++ src/elm/InBasketFlow.elm
@@ -1,36 +1,40 @@
 module InBasketFlow exposing (..)
 
 import DebugExtra.Debug exposing (tapLog)
-import InBasketFlow.Model exposing (..)
+import InBasketFlow.Model as Model exposing (Model)
 import List.Extra
 import Toolkit.Operators exposing (..)
 import Toolkit.Helpers exposing (..)
 
 
 rootNode =
-    branchNode "Is it Actionable ?"
-        (branchNode "Can be done under 2 mins?"
-            (confirmActionNode "Do it now?"
-                (actionNode "Timer Started, Go Go Go !!!")
+    Model.branchNode "Is it Actionable ?"
+        (Model.branchNode "Can be done under 2 mins?"
+            (Model.confirmActionNode "Do it now?"
+                (Model.actionNode "Timer Started, Go Go Go !!!")
             )
-            (actionNode "Involves Multiple Steps?")
+            (Model.actionNode "Involves Multiple Steps?")
         )
-        (branchNode "Is it worth keeping?"
-            (branchNode "Could Require actionNode Later ?"
-                (actionNode "Move to SomDay/Maybe List?")
-                (actionNode "Move to Reference?")
+        (Model.branchNode "Is it worth keeping?"
+            (Model.branchNode "Could Require actionNode Later ?"
+                (Model.actionNode "Move to SomDay/Maybe List?")
+                (Model.actionNode "Move to Reference?")
             )
-            (actionNode "Trash it ?")
+            (Model.actionNode "Trash it ?")
         )
 
 
 test : Maybe Model
 test =
-    init rootNode
-        |> onNo
-        ?+> onYes
-        ?+> onNo
+    Model.init rootNode
+        |> logNode "start"
+        |> Model.onNo
+        ?|> logNode "no"
+        ?+> Model.onYes
+        ?|> logNode "yes"
+        ?+> Model.onNo
+        ?|> logNode "no"
 
 
 logNode =
-    tapLog (InBasketFlow.Model.getQuestion)
+    tapLog (Model.getQuestion)

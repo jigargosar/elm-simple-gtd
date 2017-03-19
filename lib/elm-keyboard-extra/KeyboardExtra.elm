41549d005adc529142b517600afa333516d4adbc
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-19 16:57:32 +0530

    WIP

---
+++ lib/elm-keyboard-extra/KeyboardExtra.elm
@@ -0,0 +1,32 @@
+module Keyboard.Extra exposing (..)
+
+import Html exposing (Attribute)
+import Html.Events as E
+import Json.Decode as D exposing (Decoder)
+import Keyboard
+import Keyboard.Extra as Keyboard exposing (Key)
+
+
+onKeyUp : (Key -> msg) -> Attribute msg
+onKeyUp onKeyMsg =
+    E.on "keyup" (D.map onKeyMsg Keyboard.targetKey)
+
+
+succeedIfDecodedKeyEquals key msg =
+    Keyboard.targetKey
+        |> D.andThen
+            (\actualKey ->
+                if key == actualKey then
+                    D.succeed msg
+                else
+                    D.fail "Not intrested"
+            )
+
+
+onEscape : msg -> Attribute msg
+onEscape msg =
+    E.on "keyup" (succeedIfDecodedKeyEquals Keyboard.Escape msg)
+
+
+keyUps msg =
+    Keyboard.ups (Keyboard.fromCode >> msg)

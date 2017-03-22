63efcbe6b1abeae57e105deeb9ca4e70b56a5f0e
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-22 17:23:04 +0530

    Refactoring step 1

--- src/elm/Main/View.elm
+++ src/elm/Main/View.elm
@@ -1,11 +1,12 @@
 module Main.View exposing (appView)
 
 import Html.Attributes.Extra exposing (..)
+import Html.Keyed as Keyed
 import KeyboardExtra as KeyboardExtra exposing (onEscape, onKeyUp)
 import Polymer.Attributes exposing (icon)
 import Toolkit.Helpers exposing (..)
 import Toolkit.Operators exposing (..)
-import Html exposing (div, hr, node, span, text)
+import Html exposing (Html, div, hr, node, span, text)
 import Html.Attributes exposing (attribute, autofocus, class, classList, id, style, value)
 import Html.Events exposing (..)
 import DebugExtra.Debug exposing (tapLog)
@@ -16,8 +17,8 @@ import Json.Encode
 import List.Extra as List
 import Main.Model exposing (..)
 import Main.Msg exposing (..)
-import Todo as Todo exposing (EditMode(..), Group(Inbox))
-import TodoStore.View
+import Todo as Todo exposing (EditMode(..), Group(Inbox), Todo, TodoId)
+import TodoStore.View exposing (ViewConfig)
 import Flow.Model as Flow exposing (Node)
 import InboxFlow
 import InboxFlow.View
@@ -25,6 +26,7 @@ import Polymer.Paper as Paper exposing (badge, button, fab, iconButton, item, it
 import Polymer.App exposing (..)
 import FunctionExtra exposing (..)
 import Main.View.DrawerMenu exposing (appDrawerMenuView)
+import Todo.View
 
 
 createTodoListViewConfig : Model -> TodoStore.View.ViewConfig Msg
@@ -81,8 +83,6 @@ drawerLayoutView m =
         ]
 
 
-
-
 addTodoView editMode =
     case editMode of
         EditNewTodoMode text ->
@@ -108,11 +108,6 @@ addNewTodoView text =
         []
 
 
-
---addTodoButton viewConfig =
---    node "paper-fab" [ id "add-fab", attribute "icon" "add", onClick viewConfig.onAddTodoClicked ] []
-
-
 centerView m =
     case getViewState m of
         TodoListViewState ->
@@ -123,5 +118,58 @@ centerView m =
 
 
 todoListView m =
-    --TodoStore.View.allTodosView (createTodoListViewConfig m) (getTodoCollection m)
-    div [] []
+    allTodosView (createTodoListViewConfig m) m
+
+
+
+--    div [] []
+
+
+allTodosView : ViewConfig msg -> Model -> Html msg
+allTodosView vc model =
+    let
+        todoListViewsWithKey : List ( String, Html msg )
+        todoListViewsWithKey =
+            model |> getTodoList >> Todo.todoListsByType .|> todoListViewWithKey (todoView vc)
+    in
+        Keyed.node "div" [] todoListViewsWithKey
+
+
+todoListViewWithKey todoView ( listTitle, todoList ) =
+    ( listTitle
+    , div [ class "todo-list-container" ]
+        [ div [ class "todo-list-title" ]
+            [ div [ class "paper-badge-container" ]
+                [ span [] [ text listTitle ]
+                , badge [ intProperty "label" (List.length todoList) ] []
+                ]
+            ]
+        , Keyed.node "paper-material" [ class "todo-list" ] (todoList .|> todoView)
+        ]
+    )
+
+
+todoView : ViewConfig msg -> Todo -> ( TodoId, Html msg )
+todoView vc todo =
+    let
+        todoId =
+            Todo.getId todo
+
+        notEditingView =
+            Todo.View.todoViewNotEditing vc todo
+
+        editingView todo =
+            Todo.View.todoViewEditing vc todo
+
+        todoViewHelp =
+            case vc.editMode of
+                EditTodoMode editingTodo ->
+                    if Todo.equalById editingTodo todo then
+                        editingView editingTodo
+                    else
+                        notEditingView
+
+                _ ->
+                    notEditingView
+    in
+        ( todoId, todoViewHelp )

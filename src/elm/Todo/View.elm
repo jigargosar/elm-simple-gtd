08a214d9651fc58ee97da6f99b0c8b85d9e9f487
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-22 14:42:09 +0530

    WIP

--- src/elm/Todo/View.elm
+++ src/elm/Todo/View.elm
@@ -21,6 +21,43 @@ todoView viewConfig (( editing, todo ) as editingTodoTuple) =
     todoItemView viewConfig editingTodoTuple
 
 
+todoViewEditing vc todo =
+    item [ class "todo-item" ]
+        [ checkbox [ checked False ] []
+        , itemBody []
+            [ input
+                [ id (todoInputId todo)
+                , class "edit-todo-input"
+                , boolProperty "noLabelFloat" True
+                , value (Todo.getText todo)
+                , onInput vc.onEditTodoTextChanged
+                , onBlur vc.onEditTodoBlur
+                , onKeyUp vc.onEditTodoKeyUp
+                , autofocus True
+                ]
+                []
+            ]
+        , hoverIcons vc todo
+        ]
+
+
+todoViewNotEditing vc todo =
+    item
+        [ class "todo-item"
+        , onClick (vc.onEditTodoClicked (todoInputId todo) todo)
+        ]
+        [ checkbox [ checked False ] []
+        , itemBody []
+            [ span [ class "ellipsis" ] [ Todo.getText todo |> text ]
+            , span [ class "small dim" ]
+                [ text ("created " ++ (Todo.createdAtInWords vc.now todo) ++ " ago. ")
+                , text ("modified " ++ (Todo.modifiedAtInWords vc.now todo) ++ " ago")
+                ]
+            ]
+        , hoverIcons vc todo
+        ]
+
+
 todoInputId todo =
     "edit-todo-input-" ++ (Todo.getId todo)
 
@@ -50,32 +87,34 @@ todoItemBody editing vc todo =
             ]
 
 
+hoverIcons vc todo =
+    div [ class "hover hover-icons" ]
+        [ doneIconButton vc todo
+        , deleteIconButton vc todo
+        , optionsIconButton vc todo
+        ]
+
+
 todoItemView vc ( editing, todo ) =
     let
-        hoverIcons =
-            div [ class "hover hover-icons" ]
-                [ doneIconButton vc todo
-                , deleteIconButton vc todo
-                , optionsIconButton vc todo
-                ]
-
-        onEditTodoClicked =
-            onClick (vc.onEditTodoClicked (todoInputId todo) todo)
-
         itemOptionalAttributes =
             if editing then
                 []
             else
-                [ onEditTodoClicked ]
+                [ onClick (vc.onEditTodoClicked (todoInputId todo) todo) ]
 
         itemAttributes =
             [ class "todo-item" ] ++ itemOptionalAttributes
     in
-        item itemAttributes
-            [ checkbox [ checked False ] []
-            , todoItemBody editing vc todo
-            , hoverIcons
-            ]
+        todoItemViewHelp itemAttributes editing vc todo
+
+
+todoItemViewHelp itemAttributes editing vc todo =
+    item itemAttributes
+        [ checkbox [ checked False ] []
+        , todoItemBody editing vc todo
+        , hoverIcons vc todo
+        ]
 
 
 doneIconButton vc todo =

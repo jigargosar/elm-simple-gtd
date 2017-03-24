8c6245115d25e91c9fedaba7a64cc1314d5948c5
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-24 21:07:03 +0530

    WIP

--- src/elm/Main/Msg.elm
+++ src/elm/Main/Msg.elm
@@ -11,6 +11,7 @@ import Navigation exposing (Location)
 import Time exposing (Time)
 import Todo exposing (Todo, TodoGroup, TodoId)
 import FunctionExtra.Operators exposing (..)
+import Function
 
 
 type NewTodoMsg

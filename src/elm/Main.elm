8c57453a4396071d8ffbdad1913c325c5a6b68fc
Jigar Gosar <jigar.gosar@gmail.com>
2017-03-29 15:31:22 +0530

    WIP

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -28,6 +28,7 @@ import Function exposing ((>>>))
 import Html
 import Types exposing (..)
 import RunningTodoDetails
+import Update
 
 
 type alias Flags =
@@ -40,7 +41,7 @@ main =
         { delta2url = Routes.delta2hash
         , location2messages = Routes.hash2messages
         , init = init
-        , update = update
+        , update = Update.update
         , view = appView
         , subscriptions = \m -> Sub.batch [ Time.every Time.second (OnUpdateNow) ]
         }
@@ -56,56 +57,3 @@ init { now, encodedTodoList } =
         (Random.seedFromTime now)
         RunningTodoDetails.init
         |> Return.singleton
-
-
-update : Msg -> Model -> Return
-update msg =
-    Return.singleton
-        >> case msg of
-            NoOp ->
-                identity
-
-            OnEditModeMsg msg ->
-                Update.EditModeUpdate.onEditModeMsg msg
-
-            OnTodoMsg msg ->
-                Update.TodoUpdate.update msg
-
-            SetMainViewType viewState ->
-                Return.map (Model.setMainViewType viewState)
-
-            OnUpdateNow now ->
-                onUpdateNow now
-
-            OnMsgList messages ->
-                onMsgList messages
-
-
-onMsgList : List Msg -> ReturnF
-onMsgList =
-    flip (List.foldl (update >> Return.andThen))
-
-
-andThenUpdate =
-    update >> Return.andThen
-
-
-onUpdateNow now =
-    Return.map (Model.setNow now)
-        >> Return.andThen
-            (\m ->
-                let
-                    shouldBeep =
-                        Model.RunningTodo.shouldBeep m
-                in
-                    if shouldBeep then
-                        ( Model.RunningTodo.updateLastBeepedTo now m, startAlarm () )
-                    else
-                        Return.singleton m
-            )
-
-
-port startAlarm : () -> Cmd msg
-
-
-port stopAlarm : () -> Cmd msg
---
+++ src/elm/Update.elm
@@ -0,0 +1,83 @@
+port module Update exposing (..)
+
+import Dom
+import Update.EditModeUpdate
+import Model.EditMode
+import Model.RunningTodo
+import RandomIdGenerator as Random
+import Random.Pcg as Random exposing (Seed)
+import FunctionExtra exposing (..)
+import Json.Encode as E
+import Keyboard.Extra exposing (Key(Enter, Escape))
+import Model as Model
+import Routes
+import View exposing (appView)
+import Navigation exposing (Location)
+import Return
+import RouteUrl exposing (RouteUrlProgram)
+import Task
+import Time exposing (Time)
+import PouchDB
+import Update.TodoUpdate exposing (..)
+import Toolkit.Operators exposing (..)
+import Toolkit.Helpers exposing (..)
+import Maybe.Extra as Maybe
+import Todo as Todo exposing (EncodedTodoList, Todo, TodoId)
+import Tuple2
+import Function exposing ((>>>))
+import Html
+import Types exposing (..)
+import RunningTodoDetails
+
+
+update : Msg -> Model -> Return
+update msg =
+    Return.singleton
+        >> case msg of
+            NoOp ->
+                identity
+
+            OnEditModeMsg msg ->
+                Update.EditModeUpdate.onEditModeMsg msg
+
+            OnTodoMsg msg ->
+                Update.TodoUpdate.update msg
+
+            SetMainViewType viewState ->
+                Return.map (Model.setMainViewType viewState)
+
+            OnUpdateNow now ->
+                onUpdateNow now
+
+            OnMsgList messages ->
+                onMsgList messages
+
+
+onMsgList : List Msg -> ReturnF
+onMsgList =
+    flip (List.foldl (update >> Return.andThen))
+
+
+andThenUpdate =
+    update >> Return.andThen
+
+
+onUpdateNow now =
+    Return.map (Model.setNow now)
+        >> Return.andThen
+            (\m ->
+                let
+                    shouldBeep =
+                        Model.RunningTodo.shouldBeep m
+                in
+                    if shouldBeep then
+                        ( Model.RunningTodo.updateLastBeepedTo now m, startAlarm () )
+                    else
+                        Return.singleton m
+            )
+
+
+port startAlarm : () -> Cmd msg
+
+
+port stopAlarm : () -> Cmd msg

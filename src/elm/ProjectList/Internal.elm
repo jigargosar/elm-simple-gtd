bb673e439d0bcb0df5568eb3245ee86669609b87
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-03 16:07:46 +0530

    WIP

--- src/elm/ProjectList/Internal.elm
+++ src/elm/ProjectList/Internal.elm
@@ -1,10 +1,39 @@
 module ProjectList.Internal exposing (..)
 
+import Project exposing (Project)
+import ProjectList.Types exposing (..)
 import Toolkit.Helpers exposing (..)
 import Toolkit.Operators exposing (..)
 import Ext.Function exposing (..)
 import Ext.Function.Infix exposing (..)
+import Random.Pcg exposing (Seed)
 
 
-_ =
-    1
+getSeed : Model -> Seed
+getSeed =
+    (.seed)
+
+
+setSeed : Seed -> ModelF
+setSeed seed model =
+    { model | seed = seed }
+
+
+updateSeed : (Model -> Seed) -> ModelF
+updateSeed updater model =
+    setSeed (updater model) model
+
+
+getList : Model -> List Project
+getList =
+    (.list)
+
+
+setList : List Project -> ModelF
+setList list model =
+    { model | list = list }
+
+
+updateList : (Model -> List Project) -> ModelF
+updateList updater model =
+    setList (updater model) model

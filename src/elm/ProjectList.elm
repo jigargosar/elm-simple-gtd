6e8a4ce8925b3a00e0193eae21257d06b1fe6191
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-03 16:19:35 +0530

    WIP

--- src/elm/ProjectList.elm
+++ src/elm/ProjectList.elm
@@ -1,7 +1,8 @@
 module ProjectList exposing (..)
 
-import Project
+import Project exposing (Project, ProjectName)
 import ProjectList.Types exposing (..)
+import ProjectList.Internal exposing (..)
 import Toolkit.Helpers exposing (..)
 import Toolkit.Operators exposing (..)
 import Ext.Function exposing (..)
@@ -9,6 +10,8 @@ import Ext.Function.Infix exposing (..)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
 import Json.Encode as E
+import List.Extra as List
+import Random.Pcg as Random
 
 
 decodeProjectList : EncodedProjectList -> ProjectList
@@ -27,3 +30,40 @@ decodeProjectList =
                         in
                             Nothing
             )
+
+
+getEncodedProjectNames =
+    List.map (Project.getName >> Json.Encode.string) >> Json.Encode.list
+
+
+getProjectIdByName =
+    findProjectByName >>> Maybe.map Project.getId
+
+
+findProjectByName projectName =
+    List.find (Project.nameEquals projectName)
+
+
+findProjectById id =
+    List.find (Project.getId >> equals id)
+
+
+addNewProject : ProjectName -> Model -> ( Project, Model )
+addNewProject projectName now =
+    generate (Project.projectGenerator projectName now)
+        >> addProjectFromTuple
+
+
+generate : Random.Generator a -> Model -> ( a, Model )
+generate generatorFn m =
+    Random.step (generatorFn m) (getSeed m)
+        |> Tuple.mapSecond (setSeed # m)
+
+
+addProjectFromTuple : ( Project, Model ) -> ( Project, Model )
+addProjectFromTuple =
+    apply2 ( Tuple.first, uncurry addProject )
+
+
+addProject project =
+    updateList (getList >> (::) project)

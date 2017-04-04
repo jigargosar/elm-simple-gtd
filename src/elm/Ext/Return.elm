0e87cee0d72ca9b0c0c62a1a3849b31177ec2cf4
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-04 18:11:15 +0530

    WIP

--- src/elm/Ext/Return.elm
+++ src/elm/Ext/Return.elm
@@ -7,14 +7,6 @@ import Toolkit.Operators exposing (..)
 import Ext.Function exposing (..)
 
 
-transformModelTupleWith f =
-    Return.andThen (\( x, m ) -> (f x) (Return.singleton m))
-
-
-transformMaybeModelTupleWith f =
-    transformModelTupleWith (Maybe.unwrap identity f)
-
-
 maybeTransformWith :
     (a -> Maybe x)
     -> (x -> ReturnF msg a)
@@ -24,20 +16,6 @@ maybeTransformWith f1 f2 =
         (\m ->
             f1 m ?|> f2 ?= identity |> apply (Return.singleton m)
         )
-
-
-andThenMaybe f =
-    Return.andThen (\m -> f m ?= Return.singleton m)
-
-
-transformWithComplexImplementation :
-    (a -> x)
-    -> (x -> Return msg a -> Return msg b)
-    -> Return msg a
-    -> Return msg b
-transformWithComplexImplementation f1 f2 =
-    Return.map (apply2 ( f1, identity ))
-        >> Return.andThen (\( x, m ) -> (f2 x) (Return.singleton m))
 
 
 transformWith :
@@ -59,7 +37,7 @@ with f1 f2 =
         )
 
 
-maybeAndThenWith f1 f2 =
+maybeWith f1 f2 =
     Return.andThen
         (\m ->
             (f2 (f1 m)) m ?= Return.singleton m

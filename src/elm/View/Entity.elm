cf3679c07d0534705306d5ed9b29bd436f366918
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-12 16:01:34 +0530

    Feature: Edit/Delete Project/Context

--- src/elm/View/Entity.elm
+++ src/elm/View/Entity.elm
@@ -29,6 +29,7 @@ type alias ViewModel =
 
 type alias ModelConfig a =
     { createEntity : Entity
+    , entityType : EntityType
     , getId : a -> String
     , isNull : a -> Bool
     , getName : a -> String
@@ -80,6 +81,7 @@ createProjectVMs model =
             |> (::) Project.null
             .|> createVM todoListByEntityId
                     { createEntity = ProjectEntity
+                    , entityType = ProjectEntityType
                     , getId = Project.getId
                     , isNull = Project.isNull
                     , getName = Project.getName
@@ -97,6 +99,7 @@ createContextVMS model =
             |> (::) Context.null
             .|> createVM todoListByEntityId
                     { createEntity = ContextEntity
+                    , entityType = ContextEntityType
                     , getId = Context.getId
                     , isNull = Context.isNull
                     , getName = Context.getName

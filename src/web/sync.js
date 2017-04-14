8765ebc93f9d92b4ab3c4f0c17b3379d26e48641
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-14 19:36:15 +0530

    experimenting with peerjs and webrtc

--- src/web/sync.js
+++ src/web/sync.js
@@ -2,38 +2,64 @@
 import Peer from "peerjs"
 
 export default function (app) {
+    let peer = createPeer()
 
-    app.ports["startSync"].subscribe(([myPeerId, remotePeerId])=>{
 
-        // const myPeerId = localStorage.getItem("my-peer-id")
-        localStorage.setItem("my-peer-id", myPeerId)
-        localStorage.setItem("remote-peer-id", remotePeerId)
+    function createPeer() {
+        const myPeerId = localStorage.getItem("my-peer-id")
 
+        if (peer && peer.id !== myPeerId) {
+            peer.destroy();
+            return createPeer2(myPeerId)
+        }
+        if (!peer) {
+            return createPeer2(myPeerId)
+        }
+        return peer;
+
+    }
+
+
+    function createPeer2(myPeerId) {
         const peer = new Peer(myPeerId, {secure: true, host: 'sgtd-peer-js.herokuapp.com', port: ''});
         peer.on("error", e => {
             console.dir(e)
-            if (e.type === "network") {
+            if (e.type === "network" && !peer.destroyed) {
                 setTimeout(() => {
                     peer.reconnect()
                 }, 5000)
-            } 
+            }
         })
         peer.on('open', function (id) {
             console.log('My peer ID is: ' + id);
         });
+        return peer;
+    }
 
-        const conn =  peer.connect(remotePeerId)
-        conn.on('open', function() {
+    app.ports["startSync"].subscribe(([myPeerId, remotePeerId]) => {
+
+        localStorage.setItem("my-peer-id", myPeerId)
+        localStorage.setItem("remote-peer-id", remotePeerId)
+        const oldPeer = peer
+
+        peer = createPeer()
+
+        if(oldPeer !== peer)
+        {
+            debugger
+        }
+        const conn = peer.connect(remotePeerId)
+        conn.on('open', function () {
             conn.send('ping');
         });
-        conn.on('data', function(data) {
+        conn.on('data', function (data) {
             console.log('Received', data);
         });
-        conn.on("error", e =>{
+        conn.on("error", e => {
             console.dir(e)
             console.error("open error", e)
         })
-        conn.on("close", e =>{
+        conn.on("close", e => {
             console.dir(e)
             console.error("closed", e)
         })
@@ -52,7 +78,6 @@ export default function (app) {
                 console.error("in coming conn closed")
             })
         });
-
 
 
     })

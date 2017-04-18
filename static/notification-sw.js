a3f5ce0924f8da08a8cd8b6c26e1ad89869ef9b8
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-18 18:06:38 +0530

    add date time picker

--- static/notification-sw.js
+++ static/notification-sw.js
@@ -11,6 +11,28 @@
 
 self.addEventListener('notificationclick', function (event) {
     console.log("notification click", event)
-
     event.notification.close();
+
+
+    event.waitUntil(
+        clients
+            .matchAll({type: "window"})
+            .then(function (clientList) {
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
+                    client.postMessage("notification clicked")
+                    if (client.focus) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients
+                        .openWindow('http://localhost:8020/')
+                        .then(windowClient => {
+                            windowClient.postMessage("notification clicked")
+                        })
+                }
+            })
+    );
+
 }, false);

ec89f099551f3eacfb2a1aa0d1538000c46fa540
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-20 14:00:57 +0530

    reminder notification

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -153,7 +153,7 @@ appMainView contextVMs projectVMs m =
 
 
 notificationView todo =
-    div [ class "fixed-bottom top-shadow bkg-white static" ]
+    div [ class "fixed-bottom top-shadow static" ]
         [ div [ class "font-headline" ] [ todo |> Todo.getText >> text ]
         , div [ class "layout horizontal flex-auto-children" ]
             [ iconTextButton "cancel" "dismiss" Msg.NoOp

ee35ae946ffde19918acc4809045b23e34838fbd
Jigar Gosar <jigar.gosar@gmail.com>
2017-04-25 20:59:08 +0530

    edit todo UI simplification

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -255,8 +255,8 @@ update msg =
 
                 SaveEditingEntity ->
                     Return.map (Model.saveEditModeEntity)
+                        >> andThenUpdate DeactivateEditingMode
 
-                --                        >> andThenUpdate DeactivateEditingMode
                 OnEntityAction entity action ->
                     case (action) of
                         StartEditing ->

1a2976090d7f34999ea1c72a53564cd26a4967de
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-01 13:52:43 +0530

    upgrading to p2x

--- src/elm/Test/View.elm
+++ src/elm/Test/View.elm
@@ -10,16 +10,35 @@ import Ext.Function exposing (..)
 import Ext.Function.Infix exposing (..)
 import List.Extra as List
 import Maybe.Extra as Maybe
-import String.Extra
+import String.Extra as String
 
 
-createItems =
-    List.range 0 10 .|> createItem
+createItems model =
+    model.list .|> createItem model.selectedIdx
 
 
-createItem idx =
-    li [ tabindex 1 ] [ idx |> toString >> String.append "item no: " >> text ]
+createItem selectedIdx idx =
+    let
+        tabIndexValue =
+            if idx == selectedIdx then
+                0
+            else
+                -1
+    in
+        li [ tabindex tabIndexValue ] [ idx |> toString >> String.append "item no: " >> text ]
 
 
 init =
-    ul [] createItems
+    ul [] (createItems createModel)
+
+
+createModel =
+    { list = List.range 0 10
+    , selectedIdx = 0
+    }
+
+
+type alias Model =
+    { list : List Int
+    , selectedIdx : Int
+    }

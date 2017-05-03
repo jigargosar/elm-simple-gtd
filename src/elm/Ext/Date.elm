93379bc1d110f0c539691a17c757e716ae783363
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-03 18:30:47 +0530

    Todo view : time formatting

--- src/elm/Ext/Date.elm
+++ src/elm/Ext/Date.elm
@@ -28,16 +28,20 @@ smartFormat refDate date =
         formattedDate =
             formatDateWithoutTime date |> String.trim
 
-        dayDiff =
-            Date.diff Date.Day (Date.ceiling Date.Day refDate) (Date.ceiling Date.Day date)
+        diffInDays =
+            dayDiff refDate date
     in
-        if dayDiff == 0 then
+        if diffInDays == 0 then
             formattedTime
-        else if dayDiff == -1 then
+        else if diffInDays == -1 then
             "Yesterday " ++ formattedTime
-        else if dayDiff == 1 then
+        else if diffInDays == 1 then
             "Tomorrow " ++ formattedTime
-        else if dayDiff > 1 && dayDiff < 7 then
+        else if diffInDays > 1 && diffInDays < 7 then
             (Date.format "%A " date) ++ formattedTime
         else
             formattedDate ++ " " ++ formattedTime
+
+
+dayDiff refDate date =
+    Date.diff Date.Day (Date.ceiling Date.Day refDate) (Date.ceiling Date.Day date)

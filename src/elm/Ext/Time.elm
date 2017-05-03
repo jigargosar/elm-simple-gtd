f40b8195ac18207fd6503f9091a8f3080d5869f8
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-03 17:20:59 +0530

    Todo view : time formatting

--- src/elm/Ext/Time.elm
+++ src/elm/Ext/Time.elm
@@ -13,38 +13,6 @@ import Time.Format
 import Tuple2
 
 
---toHHMMSS : Time -> String
---toHHMMSS time =
---    let
---        roundToFloat =
---            round >> toFloat
---
---        roundToString =
---            round >> toString
---
---        secondsMilli =
---            time / Time.second |> roundToFloat |> (*) Time.second
---
---        minutesMilli =
---            time / Time.minute |> roundToFloat |> (*) Time.minute
---
---        hoursMilli =
---            time / Time.hour |> roundToFloat |> (*) Time.hour
---
---        seconds =
---            abs (secondsMilli - minutesMilli) / Time.second |> roundToString
---
---        --        seconds =
---        --            rem (round time) (round Time.second) |> toString
---        minutes =
---            abs (minutesMilli - hoursMilli) / Time.minute |> roundToString
---
---        hours =
---            (hoursMilli) / Time.hour |> roundToString
---    in
---        [ hours, minutes, seconds ] |> String.join ":"
-
-
 toHHMMSS : Time -> String
 toHHMMSS =
     toHMSList >> List.map (toString >> String.padLeft 2 '0') >> String.join ":"

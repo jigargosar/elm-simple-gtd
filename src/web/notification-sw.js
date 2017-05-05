52f4e48f507e5e24736fd696f8a6b887e31bd010
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-05 21:25:46 +0530

    add fcm push

--- src/web/notification-sw.js
+++ src/web/notification-sw.js
@@ -62,15 +62,15 @@ firebase.initializeApp({
 // });
 
 
-// messaging.setBackgroundMessageHandler(function(payload) {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     // Customize notification here
-//     const notificationTitle = 'Background Message Title';
-//     const notificationOptions = {
-//         body: 'Background Message body.',
-//         icon: '/firebase-logo.png'
-//     };
-//
-//     return self.registration.showNotification(notificationTitle,
-//         notificationOptions);
-// });
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});

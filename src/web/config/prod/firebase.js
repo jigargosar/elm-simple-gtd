f9aff1cd0b51fd10e76272cd42da47c073747904
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-06 17:59:37 +0530

    moving firebase attributes inside elm. so that we can configure it with flags.

---
+++ src/web/config/prod/firebase.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export const appAttributes = [
+    ["database-url", "https://rational-mote-664.firebaseio.com"]
+    , ["api-key", "AIzaSyASFVPlWjIrpgSlmlEEIMZ0dtPFOuRC0Hc"]
+    , ["messaging-sender-id", "49437522774"]
+    , ["auth-domain", "rational-mote-664.firebaseapp.com"]
+]

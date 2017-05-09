e344171b9af9b50b61f5299db3d8ef8beef1e4e7
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-09 15:12:34 +0530

    storing entity list for view in model.

--- src/elm/Main/View.elm
+++ src/elm/Main/View.elm
@@ -1,5 +1,7 @@
 module Main.View exposing (..)
 
+import Document
+import Entity.ViewModel
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -15,8 +17,9 @@ import List.Extra as List
 import Maybe.Extra as Maybe
 import Model
 import Model.Internal as Model
-import Types exposing (MainViewType(..))
+import Types exposing (Entity(..), MainViewType(..))
 import View.TodoList
+import ViewModel exposing (EntityView(..))
 
 
 init viewModel model =
@@ -26,17 +29,42 @@ init viewModel model =
 
         contextVMs =
             viewModel.contexts.entityList
+
+        getMaybeContextVM context =
+            contextVMs |> List.find (.id >> equals (Document.getId context))
+
+        getMaybeProjectVM project =
+            projectVMs |> List.find (.id >> equals (Document.getId project))
+
+        entityList =
+            Model.getViewEntityList model
+
+        entityViewList : List EntityView
+        entityViewList =
+            entityList
+                .|> (\entity ->
+                        case entity of
+                            ContextEntity context ->
+                                getMaybeContextVM context ?|> EntityView
+
+                            ProjectEntity project ->
+                                getMaybeProjectVM project ?|> EntityView
+
+                            TodoEntity todo ->
+                                TodoView todo |> Just
+                    )
+                |> List.filterMap identity
     in
         div [ id "main-view" ]
             [ case Model.getMainViewType model of
                 GroupByContextView ->
-                    View.TodoList.groupByEntity viewModel viewModel.contexts.entityList model
+                    View.TodoList.listView entityViewList viewModel model
 
                 GroupByProjectView ->
                     View.TodoList.groupByEntity viewModel viewModel.projects.entityList model
 
                 ProjectView id ->
-                    View.TodoList.groupByEntityWithId viewModel projectVMs id model
+                    View.TodoList.listView entityViewList viewModel model
 
                 ContextView id ->
                     View.TodoList.groupByEntityWithId viewModel contextVMs id model

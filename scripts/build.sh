153ef82515eb11f63c0a831e67fbc0c5e8c4dff0
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-12 16:38:14 +0530

    update travis config with elm-make timeout workaround   Reference: https://github.com/elm-lang/elm-compiler/issues/1473#issuecomment-245704142

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cross-env NODE_ENV=production webpack -p --progress &&
-cp -rv static/** app/ &&
+cp -Rv static/** app/ &&
 #bash scripts/polymer-build.sh app &&
 cd app &&
 polymer --version &&
 polymer build
-cp -rv app/build/unbundled/** docs/
+cp -Rv app/build/unbundled/** docs/

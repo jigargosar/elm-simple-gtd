8b9c3728239e242cbc65169cd89f5a4b8b0d5d90
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-13 17:15:26 +0530

    deploy docs: delay fb messaging activation

--- src/web/notification-sw.js
+++ src/web/notification-sw.js
@@ -51,8 +51,8 @@ function postMessage(client, event) {
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.

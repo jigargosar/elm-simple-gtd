f4a8ad290cc305d79817b31d382548bd7932fb25
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-13 15:40:35 +0530

    [fix] polymer build issue

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-pwd &&
-cp -rf static/ app &&
-rm -rf app/bower_components &&
-ls -al app &&
-cp -rf src/web/bower_components app/bower_components &&
-ls -al app &&
-echo "content of app/bower_components" &&
-ls -al app/bower_components &&
-echo "content of app/bower_components/paper-styles/element-styles/" &&
-ls -al app/bower_components/paper-styles/element-styles/ &&
+cp -R static/ app &&
 cross-env NODE_ENV=production webpack -p --progress &&
 cd app &&
-pwd &&
-ls -al . &&
-ls -al bower_components &&
 polymer --version &&
-(polymer build || ( ls -al bower_components && pwd && false ))
+polymer build &&
 cd .. &&
-cp -Rvf app/build/unbundled/ docs
+cp -R app/build/unbundled/ docs

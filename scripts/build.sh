0f9d1c1a197a878eaa0d6e387e513d81f753e394
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-13 12:11:41 +0530

    deploy v8 * prod fcm debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 cp -Rvf static/ app &&
+ls -al app/bower_components &&
 cross-env NODE_ENV=production webpack -p --progress &&
 #bash scripts/polymer-build.sh app &&
+ls -al app/bower_components &&
 cd app &&
 polymer --version &&
-polymer build
+polymer build &&
 cd .. &&
 cp -Rvf app/build/unbundled/ docs

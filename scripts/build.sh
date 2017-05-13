e1bb62c3c9829f40cae505c3f32559629980d799
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-13 14:20:07 +0530

    continue debugging travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 pwd &&
-cp -Rf static/ app &&
+cp -rf static/ app &&
 rm -f app/bower_components &&
-cp -R src/web/bower_components app/bower_components &&
+ls -al app &&
+cp -rf src/web/bower_components app/bower_components &&
+ls -al app &&
 cross-env NODE_ENV=production webpack -p --progress &&
 cd app &&
+ls -al . &&
 polymer --version &&
 polymer build &&
 cd .. &&

9e3490cd5a90599ee87361f4dad73cf0e11fa51d
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-13 13:50:23 +0530

    debug travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-cwd &&
-cp -Rvf static/ app &&
-ls -al app/bower_components &&
+pwd &&
+cp -Rf static/ app &&
+rm -f app/bower_components &&
+cp -R src/web/bower_components app/bower_components &&
 cross-env NODE_ENV=production webpack -p --progress &&
-#bash scripts/polymer-build.sh app &&
-ls -al app/bower_components &&
-rm -f app/bower_components
-cp -Rvf src/web/bower_components app/
-ls -al /home/travis/build/jigargosar/elm-simple-gtd/app/bower_components/paper-styles/element-styles/paper-material.html &&
-ls -al app/bower_components &&
 cd app &&
-cwd &&
 polymer --version &&
 polymer build &&
 cd .. &&
 cp -Rvf app/build/unbundled/ docs
+true

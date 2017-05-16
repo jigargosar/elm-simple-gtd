59ce7b0c5bc5c8e4ac3a7293dc764ca9d3619523
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-16 15:08:56 +0530

    [fix] fcmToken: fire functions: use multiple tokens per user

--- functions/index.js
+++ functions/index.js
@@ -87,7 +87,7 @@ function sendPushNotifications(notificationMap) {
 }
 
 const sendPush = notificationData => tokenMap => {
-    console.debug("tokenMap", tokenMap)
+    console.log("tokenMap", tokenMap)
     const {todoId, timestamp, uid} = notificationData
     const sendPushForDevice = (token, deviceId)=>{
         return admin

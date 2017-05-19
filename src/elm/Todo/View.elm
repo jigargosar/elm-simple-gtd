3ce5d24b4ea420f8c5d161bdb5d4449341d2b3af
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-19 18:01:27 +0530

    [fix] in paper-input space key should not preventDefault. pD was intended only to stop scrolling on space key

--- src/elm/Todo/View.elm
+++ src/elm/Todo/View.elm
@@ -496,7 +496,10 @@ editView edit =
                 [ class "auto-focus"
                 , stringProperty "label" "Todo"
                 , value (edit.todo.text)
+                -- for when we need custom handling for enter key
                 , property "keyBindings" Json.Encode.null
+
+                -- so that space key doesn't buble up
                 , boolProperty "stopKeyboardEventPropagation" True
                 , onInput edit.onTodoTextChanged
                 ]

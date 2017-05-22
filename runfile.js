ff774ef613f5dc256d613b0c4642b8ec8d5e388d
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-22 13:58:00 +0530

    using runjs to manage npm scripts

---
+++ runfile.js
@@ -0,0 +1,30 @@
+"use strict"
+import {run} from 'runjs'
+
+export const docs = {
+    gitStatus(){
+        run("git status docs")
+    },
+    commit(){
+        run("git unstage .")
+        run("git add docs/**")
+        run("git commit -m '[npm-auto-commit] deploy docs'")
+    }
+}
+export const build = {
+    commitDocs(){
+        docs.gitStatus()
+        docs.commit()
+        docs.gitStatus()
+    }
+}
+
+export function createcomponent(name) {
+
+}
+
+export function lint(path = '.') {
+    this.options.fix ? run(`eslint ${path} --fix`) : run(`eslint ${path}`)
+}
+
+lint.help = 'Do linting for javascript files'

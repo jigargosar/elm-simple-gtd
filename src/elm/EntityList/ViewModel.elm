e0d276989bccf643ffded3b6af024c46e48f07e1
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-23 18:23:28 +0530

    refactoring: cleanup View.TodoList

--- src/elm/EntityList/ViewModel.elm
+++ src/elm/EntityList/ViewModel.elm
@@ -1,5 +1,6 @@
 module EntityList.ViewModel exposing (..)
 
+import Entity exposing (Entity)
 import Model
 import Toolkit.Helpers exposing (..)
 import Toolkit.Operators exposing (..)
@@ -9,5 +10,11 @@ import List.Extra as List
 import Maybe.Extra as Maybe
 
 
+type alias ViewModel =
+    { entityList : List Entity
+    }
+
+
+create : Entity.ListViewType -> Model.Model -> ViewModel
 create viewType model =
     { entityList = Model.createViewEntityList viewType model }

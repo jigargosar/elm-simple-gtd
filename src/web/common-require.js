15e7e025479ae975a896f1c14de556aa4f327864
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-27 20:32:26 +0530

    [fix] use materialize css inputs and set defaultValue to avoid cursor jump

--- src/web/common-require.js
+++ src/web/common-require.js
@@ -3,16 +3,19 @@ require("babel-polyfill")
 require("./bower_components/webcomponentsjs/webcomponents-loader")
 
 // style
+require("materialize-css/bin/materialize.css")
 require("./pcss/main.pcss")
+
 
 // lib
 require("ramda")
 require('crypto-random-string')
 require("kefir")
 
+
 // jquery
 const $ = require("jquery")
 window.jQuery = $
 require("./jquery.trap")
 require("jquery-ui/ui/position")
-
+require("materialize-css")

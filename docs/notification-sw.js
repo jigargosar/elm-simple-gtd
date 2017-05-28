1e5648b1800954ed8bfa9ada8dc477fc5683f0ef
Jigar Gosar <jigar.gosar@gmail.com>
2017-05-28 18:25:17 +0530

    [runfile-commit-docs] 0.16.12

--- docs/notification-sw.js
+++ docs/notification-sw.js
@@ -1,4 +1,4 @@
-var serviceWorkerOption = {"assets":["/main.js","/common.js"]};
+var serviceWorkerOption = {"assets":["/eed9aab5449cc9c8430d7d258108f602.woff","/c0f1e4a4fdfb8048c72e86aadb2a247d.woff2","/ea36cd9a0e9eee97012a67b8a4570d7b.woff","/3c37aa69cd77e6a53a067170fa8fe2e9.woff2","/cf4d60bc0b1d4b2314085919a00e1724.woff","/1561b424aaef2f704bbd89155b3ce514.woff2","/3cf6adf61054c328b1b0ddcd8f9ce24d.woff","/5136cbe62a63604402f2fedb97f246f8.woff2","/44b78f142603eb69f593ed4002ed7a4a.woff","/1f35e6a11d27d2e10d28946d42332dc5.woff2","/main.js","/common.js"]};
         
         const isDevEnv = false;
     

e7f145becc806f8c247458174bbe8a758af8e795
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-04 13:29:52 +0530

    * quick access using launch bar

--- src/elm/LaunchBar/View.elm
+++ src/elm/LaunchBar/View.elm
@@ -19,26 +19,31 @@ import Msg exposing (commonMsg)
 init m =
     case Model.getEditMode m of
         EditMode.LaunchBar form ->
-            div
-                [ id "modal-background"
-                , onKeyDownStopPropagation (\_ -> commonMsg.noOp)
-                , onClickStopPropagation Msg.DeactivateEditingMode
-                ]
-                [ div
-                    [ id "launch-bar-container"
-                    , attribute "onclick" "console.log('focusing');document.getElementById('hidden-input').focus(); event.stopPropagation(); event.preventDefault();"
-                    , onInput (Msg.UpdateLaunchBarInput form)
-                    ]
-                    [ div [] [ text "Input:", text form.input ]
-                    , input
-                        [ id "hidden-input"
-                        , class "auto-focus"
-                        , autofocus True
-                        , value form.input
-                        ]
-                        []
-                    ]
-                ]
+            formView form m
 
         _ ->
             span [] []
+
+
+formView form m =
+    div
+        [ id "modal-background"
+        , onKeyDownStopPropagation (\_ -> commonMsg.noOp)
+        , onClickStopPropagation Msg.DeactivateEditingMode
+        ]
+        [ div
+            [ id "launch-bar-container"
+            , attribute "onclick"
+                "console.log('focusing');document.getElementById('hidden-input').focus(); event.stopPropagation(); event.preventDefault();"
+            , onInput (Msg.UpdateLaunchBarInput form)
+            ]
+            [ div [] [ text "Input:", text form.input ]
+            , input
+                [ id "hidden-input"
+                , class "auto-focus"
+                , autofocus True
+                , value form.input
+                ]
+                []
+            ]
+        ]

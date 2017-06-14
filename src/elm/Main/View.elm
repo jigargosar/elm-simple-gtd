c49a5ae62f2c0ef8556d5212c7311550c7d90c02
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-14 15:08:56 +0530

    custom-sync view: remove paper input

--- src/elm/Main/View.elm
+++ src/elm/Main/View.elm
@@ -3,6 +3,7 @@ module Main.View exposing (..)
 import Context
 import Document
 import EntityList.View
+import Ext.Keyboard exposing (onKeyDownStopPropagation)
 import Menu
 import OldGroupEntity.ViewModel
 import Html exposing (..)
@@ -36,15 +37,18 @@ init viewModel model =
                         Model.getRemoteSyncForm model
                 in
                     div [ class "z-depth-2 static layout vertical" ]
-                        [ Paper.input
-                            [ attribute "label" "Cloudant URL or any CouchDB URL"
-                            , value form.uri
-                            , onInput (Model.UpdateRemoteSyncFormUri form)
+                        [ div [ class "input-field", onKeyDownStopPropagation (\_ -> Model.NOOP) ]
+                            [ input
+                                [ defaultValue form.uri
+                                , autofocus True
+                                , onInput (Model.UpdateRemoteSyncFormUri form)
+                                ]
+                                []
+                            , label [ class "active" ] [ text "Cloudant or any CouchDB URL" ]
                             ]
-                            []
                         , div []
                             [ Paper.button [ form |> Model.RemotePouchSync >> onClick ]
-                                [ text "Start Sync" ]
+                                [ text "Sync Now" ]
                             ]
                         ]
         ]

975db7eb6780b2dfcb0d45533f26de43931f0d9f
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-18 17:48:27 +0530

    * app drawer: create model & update functions to   * show / hide archived projects/contexts   * collapse/expand state.

--- src/elm/AppDrawer/Main.elm
+++ src/elm/AppDrawer/Main.elm
@@ -7,8 +7,15 @@ import Ext.Function exposing (..)
 import Ext.Function.Infix exposing (..)
 import List.Extra as List
 import Maybe.Extra as Maybe
+import Model
+import Time
 
 
+update :
+    (Model.Msg -> Model.ReturnF)
+    -> Time.Time
+    -> AppDrawer.Model.Msg
+    -> Model.ReturnF
 update andThenUpdate now msg =
     case msg of
         AppDrawer.Model.OnToggleExpandContextList ->

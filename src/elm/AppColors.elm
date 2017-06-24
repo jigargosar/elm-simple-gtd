7afe0e9fac4c2c2078a99454ac2579a526344279
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-24 16:31:10 +0530

    * centralize and fix app colors

--- src/elm/AppColors.elm
+++ src/elm/AppColors.elm
@@ -15,7 +15,6 @@ inboxColor =
 
 
 nullContextColor =
-    --paper-blue-a200
     Color.rgb 68 138 255
 
 
@@ -24,41 +23,27 @@ contextsColor =
 
 
 defaultContextColor =
-    --paper-deep-purple-a200
     Color.rgb 124 77 255
 
 
 nullProjectColor =
-    --paper-deep-purple-200
     Color.rgb 179 157 219
 
 
 projectsColor =
-    --paper-deep-purple-a200
     Color.rgb 124 77 255
 
 
 defaultProjectColor =
-    --paper-deep-purple-a200
     Color.rgb 124 77 255
 
 
 sgtdBlue =
-    --paper-blue-a200
-    --Color.rgb 68 138 255
-    -- mat primary color
     Color.rgb 33 150 243
 
 
 defaultGroupColor =
     sgtdBlue
-
-
-
---lightGray =
---    --paper-grey-500
---    Color.rgb
---    "#9e9e9e"
 
 
 encode =

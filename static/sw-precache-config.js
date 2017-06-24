9de5dec70d746e2935d83bb4a29d75b566032a0c
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-25 02:31:21 +0530

    * test landing on dev

--- static/sw-precache-config.js
+++ static/sw-precache-config.js
@@ -1,17 +1,17 @@
 module.exports = {
     staticFileGlobs: [
         "assets/**",
-        "manifest.json",
-        "app/index.html",
         "*.ogg",
         "*.png",
         "*.ico",
+        "*.woff",
+        "*.woff2",
+        "manifest.json",
+        "app/index.html",
         "common.js",
         "vendor.js",
         "app.js",
         "landing.js",
-        "*.woff",
-        "*.woff2",
         "bower_components/pouchdb/dist/pouchdb.js"
     ],
     importScripts:["notification-sw.js"]

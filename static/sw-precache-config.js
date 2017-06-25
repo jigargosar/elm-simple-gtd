2e0c3c183993a4c0f56f6d3ed35c2b620b2a4f5d
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-25 20:36:54 +0530

    * reduce alarm file size

--- static/sw-precache-config.js
+++ static/sw-precache-config.js
@@ -1,11 +1,8 @@
 module.exports = {
     staticFileGlobs: [
         "assets/**",
-        "*.ogg",
         "*.png",
         "*.ico",
-        "*.woff",
-        "*.woff2",
         "manifest.json",
         "app/index.html",
         "common.js",

227091875ae5ace8526ad473a75c32893c23b414
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-26 15:44:43 +0530

    * cleanup, automate

--- scripts/run-elm.js
+++ scripts/run-elm.js
@@ -3,10 +3,29 @@ import * as _ from "ramda"
 import LineDriver from "line-driver"
 
 export default function () {
-    LineDriver.read( {
-        in : 'main-warn.txt',
-        line : function( props, parser ){
-            console.log( parser.line );
+    // run("elm-make --warn src/elm/Main.elm 2> main-warn.txt")
+    LineDriver.read({
+        in: 'main-warn.txt',
+        line: function (props, parser) {
+            const line = parser.line
+            console.log(parser.line);
+
+            if (props.fileName) {
+                const lineNumRegexp = /^([0-9]+)\|/
+                const match = lineNumRegexp.exec(line)
+                if (match) {
+                    run("line-replace " + props.fileName + ":" + match[1] + " '\n' ")
+                }
+
+            } else {
+                const unusedImportRegexp = /^-- unused import -* (src\/elm.*)/
+                const match = unusedImportRegexp.exec(line)
+                if (match) {
+                    console.log(match[1]);
+                    props.fileName = match[1]
+                }
+            }
+
         }
-    } );
+    });
 }

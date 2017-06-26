b2b959513c637902c3e51ac0245aa0e6830c7d7c
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-26 14:34:30 +0530

    * cleanup, automate

--- scripts/build-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp -R static/ dev &&
-cross-env NODE_ENV=development webpack --progress &&
-cd dev &&
-polymer --version &&
-polymer build &&
-cd ..
--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cp -R static/ app &&
-cross-env NODE_ENV=production webpack --progress &&
-cd app &&
-polymer --version &&
-polymer build &&
-cd .. &&
-cp -R app/build/unbundled/ docs

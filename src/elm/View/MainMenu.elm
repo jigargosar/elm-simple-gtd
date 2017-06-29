afe86e925591ff656c9bd97a0227e85575e5654a
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-29 19:47:18 +0530

    * get rid of polymer

--- src/elm/View/MainMenu.elm
+++ src/elm/View/MainMenu.elm
@@ -1,5 +1,7 @@
 module View.MainMenu exposing (..)
 
+import AppUrl
+import Firebase
 import Html
 import Menu
 import Toolkit.Helpers exposing (..)
@@ -9,23 +11,62 @@ import X.Function.Infix exposing (..)
 import List.Extra as List
 import Maybe.Extra as Maybe
 import Model
+import Tuple2
 
 
-menuConfig : Menu.State -> Model.Model -> Menu.Config String Model.Msg
-menuConfig menuState model =
-    { onSelect =
-        (\_ -> Model.noop)
+type ItemType
+    = URL String
+    | Msg Model.Msg
+
+
+type alias Item =
+    ( String, ItemType )
+
+
+menuConfig : Menu.State -> Model.Model -> Menu.Config Item Model.Msg
+menuConfig menuState appModel =
+    { onSelect = onSelect
     , isSelected = (\_ -> False)
-    , itemKey = identity
-    , itemSearchText = identity
-    , itemView = Html.text
+    , itemKey = Tuple.first
+    , itemSearchText = Tuple.first
+    , itemView = Tuple.first >> Html.text
     , onStateChanged = Model.OnMainMenuStateChanged
     , noOp = Model.noop
     , onOutsideMouseDown = Model.OnDeactivateEditingMode
     }
 
 
+onSelect ( _, itemType ) =
+    case itemType of
+        URL url ->
+            Model.OnDeactivateEditingMode
+
+        Msg msg ->
+            msg
+
+
+getItems : Model.Model -> List Item
+getItems appModel =
+    let
+        maybeUserProfile =
+            Model.getMaybeUserProfile appModel
+
+        signInMenuItem =
+            maybeUserProfile
+                ?|> (\_ -> ( "SignOut", Firebase.OnSignOut ))
+                ?= ( "SignIn", Firebase.OnSignIn )
+                |> Tuple2.map (Model.OnFirebaseMsg >> Msg)
+
+        linkMenuItems =
+            [ ( "Forums", URL AppUrl.forumsURL )
+            , ( "Changelog v" ++ appModel.appVersion, URL AppUrl.changeLogURL )
+            , ( "Github", URL AppUrl.github )
+            ]
+    in
+        signInMenuItem :: linkMenuItems
+
+
 init menuState appModel =
-    Menu.view [ "a", "b" ]
+    Menu.view (getItems appModel)
         menuState
         (menuConfig menuState appModel)

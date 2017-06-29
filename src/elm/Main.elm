27aca9be1a55a09c1d2bb10bf6c71707be1ab309
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-29 16:02:01 +0530

    * try speeding up elm compile time

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,46 +1,20 @@
 module Main exposing (..)
 
-import AppDrawer.Main
-import CommonMsg
-import Document
-import DomPorts exposing (autoFocusInputCmd, focusInputCmd, focusSelectorIfNoFocusCmd)
-import Entity.Main
-import ExclusiveMode
-import Entity
-import Firebase.Main
-import X.Debug
-import X.Keyboard as Keyboard exposing (Key)
-import X.Record as Record exposing (set)
-import X.Return as Return
 import Firebase
-import Http
 import Keyboard.Combo
-import LaunchBar
-import X.Function.Infix exposing (..)
-import Keyboard.Extra as Key
-import Model as Model
-import Notification exposing (Response)
-import Todo.Notification.Model
-import Routes
-import Store
-import Todo
-import Todo.Form
-import Todo.GroupForm
-import Todo.Msg
+import Model exposing (Flags)
 import Return
 import RouteUrl exposing (RouteUrlProgram)
-import Task
-import Time exposing (Time)
-import Toolkit.Operators exposing (..)
-import Model exposing (..)
+import Routes
+import Store
+import Time
 import Todo.Main
-import View
-import Json.Decode as D exposing (Decoder)
-import LaunchBar.Main
 import Update
+import View
+import X.Keyboard
 
 
-main : RouteUrlProgram Flags Model Msg
+main : RouteUrlProgram Flags Model.Model Model.Msg
 main =
     RouteUrl.programWithFlags
         { delta2url = Routes.delta2hash
@@ -48,33 +22,21 @@ main =
         , init = Model.init
         , update = Update.update
         , view = View.init
-        , subscriptions = subscriptions
+        , subscriptions = subscriptions2
         }
 
 
-type alias Return =
-    Return.Return Msg Model
-
-
-type alias ReturnTuple a =
-    Return.Return Msg ( a, Model )
-
-
-type alias ReturnF =
-    Return -> Return
-
-
-subscriptions : Model -> Sub Model.Msg
-subscriptions m =
+subscriptions2 : Model.Model -> Sub Model.Msg
+subscriptions2 m =
     Sub.batch
         [ Sub.batch
-            [ Time.every (Time.second * 1) OnNowChanged
-            , Keyboard.subscription OnKeyboardMsg
-            , Keyboard.ups OnGlobalKeyUp
-            , Store.onChange OnPouchDBChange
-            , Firebase.onChange OnFirebaseDatabaseChange
+            [ Time.every (Time.second * 1) Model.OnNowChanged
+            , X.Keyboard.subscription Model.OnKeyboardMsg
+            , X.Keyboard.ups Model.OnGlobalKeyUp
+            , Store.onChange Model.OnPouchDBChange
+            , Firebase.onChange Model.OnFirebaseDatabaseChange
             ]
-            |> Sub.map OnSubMsg
+            |> Sub.map Model.OnSubMsg
         , Keyboard.Combo.subscriptions m.keyComboModel
         , Todo.Main.subscriptions m
         ]

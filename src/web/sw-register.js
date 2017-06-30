a54ca2c0d460405f9b3297dce09c854c67899093
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-30 19:11:06 +0530

    * remove polymer build tools, replace with webpack sw plugin

--- src/web/sw-register.js
+++ src/web/sw-register.js
@@ -1,12 +1,6 @@
-if (IS_DEVELOPMENT_ENV) {
-    const runtime = require('serviceworker-webpack-plugin/lib/runtime')
-
-    const swScriptPath = IS_DEVELOPMENT_ENV ? "/notification-sw.js" : '/service-worker.js'
-
-    module.exports = runtime.register()
-}else{
-    module.exports = navigator.serviceWorker.register('/service-worker.js')
-}
+module.exports = IS_DEVELOPMENT_ENV ?
+    require('serviceworker-webpack-plugin/lib/runtime').register()
+    : navigator.serviceWorker.register('/service-worker.js');
 
 
 // const swScriptPath = IS_DEVELOPMENT_ENV ? "/notification-sw.js" : '/service-worker.js'

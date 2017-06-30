5cc7b7744bf740f30761882261c71deaaac5c30b
Jigar Gosar <jigar.gosar@gmail.com>
2017-06-30 19:28:26 +0530

    * remove polymer build tools, replace with webpack sw plugin

--- src/web/sw-register.js
+++ src/web/sw-register.js
@@ -1,6 +1,45 @@
+'use strict';
+
 module.exports = IS_DEVELOPMENT_ENV ?
     require('serviceworker-webpack-plugin/lib/runtime').register()
-    : navigator.serviceWorker.register('/service-worker.js');
+    : register();
+
+function register() {
+    if (!('serviceWorker' in navigator)) {
+        return Promise.reject("ServiceWorker not supported")
+    }
+    return navigator.serviceWorker.register('service-worker.js').then(function (reg) {
+        // updatefound is fired if service-worker.js changes.
+        reg.onupdatefound = function () {
+            // The updatefound event implies that reg.installing is set; see
+            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+            const installingWorker = reg.installing
+
+            installingWorker.onstatechange = function () {
+                switch (installingWorker.state) {
+                    case 'installed':
+                        if (navigator.serviceWorker.controller) {
+                            // At this point, the old content will have been purged and the fresh content will
+                            // have been added to the cache.
+                            // It's the perfect time to display a "New content is available; please refresh."
+                            // message in the page's interface.
+                            console.log('New or updated content is available.');
+                        } else {
+                            // At this point, everything has been precached.
+                            // It's the perfect time to display a "Content is cached for offline use." message.
+                            console.log('Content is now available offline!');
+                        }
+                        break;
+
+                    case 'redundant':
+                        console.error('The installing service worker became redundant.');
+                        break;
+                }
+            };
+        };
+        return reg
+    })
+}
 
 
 // const swScriptPath = IS_DEVELOPMENT_ENV ? "/notification-sw.js" : '/service-worker.js'

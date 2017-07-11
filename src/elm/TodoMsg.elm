a6f61dbc2851335bce2571a30ef176532524d27f
Jigar Gosar <jigar.gosar@gmail.com>
2017-07-11 11:08:54 +0530

    * try to reduce elm compile time, by reducing Model dependencies

--- src/elm/TodoMsg.elm
+++ src/elm/TodoMsg.elm
@@ -8,19 +8,24 @@ import X.Function exposing (..)
 import X.Function.Infix exposing (..)
 import List.Extra as List
 import Maybe.Extra as Maybe
+import Todo.Notification.Model
 
 
+onStopRunningTodo : Msg
 onStopRunningTodo =
     Todo.Msg.StopRunning |> OnTodoMsg
 
 
+onGotoRunningTodo : Msg
 onGotoRunningTodo =
     Todo.Msg.GotoRunning |> OnTodoMsg
 
 
+onReminderOverlayAction : Todo.Notification.Model.Action -> Msg
 onReminderOverlayAction =
     Todo.Msg.OnReminderOverlayAction >> OnTodoMsg
 
 
+onNewTodoForInbox : Msg
 onNewTodoForInbox =
     Todo.Msg.OnNewTodoForInbox |> OnTodoMsg

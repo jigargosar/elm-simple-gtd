af41abdee1740b7fba656ed02ffbed4eaec2604d
Jigar Gosar <jigar.gosar@gmail.com>
2017-07-19 14:45:43 +0530

    * try to reduce elm compile time

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -8,6 +8,7 @@ import Model.ViewType
 import Msg
 import TodoMsg
 import View.CustomSync
+import View.Layout
 import X.Html exposing (boolProperty, onClickStopPropagation)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -17,20 +18,6 @@ import ViewModel
 import View.Mat
 import Types.ViewType exposing (ViewType(EntityListView, SyncView))
 import View.Overlays
-
-
-init model =
-    let
-        alv =
-            appLayoutView model
-
-        children =
-            [ alv
-            , View.Mat.newTodoFab alv model
-            ]
-                ++ View.Overlays.overlayViews config model
-    in
-        div [ class "mdl-typography--body-1" ] children
 
 
 config =
@@ -51,68 +38,19 @@ config =
     , onLaunchBarMsg = Msg.OnLaunchBarMsg
     , onFirebaseMsg = Msg.OnFirebaseMsg
     , onReminderOverlayAction = TodoMsg.onReminderOverlayAction
+    , onToggleAppDrawerOverlay = Msg.onToggleAppDrawerOverlay
     }
 
 
-appLayoutView model =
+init model =
     let
-        appVM =
-            ViewModel.create model
+        alv =
+            View.Layout.appLayoutView config model
+
+        children =
+            [ alv
+            , View.Mat.newTodoFab alv model
+            ]
+                ++ View.Overlays.overlayViews config model
     in
-        if AppDrawer.Model.getIsOverlayOpen model.appDrawerModel then
-            div
-                [ id "app-layout"
-                , classList
-                    [ ( "sidebar-overlay", AppDrawer.Model.getIsOverlayOpen model.appDrawerModel )
-                    ]
-                ]
-                [ div
-                    [ id "layout-sidebar", X.Html.onClickStopPropagation Msg.noop ]
-                    [ div [ class "bottom-shadow" ] [ AppDrawer.View.sidebarHeader appVM model ]
-                    , AppDrawer.View.sidebarContent appVM model
-                    ]
-                , div
-                    [ id "layout-main"
-                    , onClick (Msg.OnAppDrawerMsg AppDrawer.Types.OnToggleOverlay)
-                    ]
-                    [ div [ X.Html.onClickStopPropagation Msg.noop ]
-                        [ div [ class "bottom-shadow" ] [ View.Header.appMainHeader appVM model ]
-                        , div [ id "layout-main-content" ] [ appMainContent model ]
-                        ]
-                    ]
-                ]
-        else
-            div
-                [ id "app-layout"
-                , classList
-                    [ ( "sidebar-overlay", AppDrawer.Model.getIsOverlayOpen model.appDrawerModel )
-                    ]
-                ]
-                [ div [ class "bottom-shadow" ]
-                    [ AppDrawer.View.sidebarHeader appVM model
-                    , View.Header.appMainHeader appVM model
-                    ]
-                , div
-                    [ id "layout-sidebar", X.Html.onClickStopPropagation Msg.noop ]
-                    [ AppDrawer.View.sidebarContent appVM model
-                    ]
-                , div
-                    [ id "layout-main"
-                    , onClick (Msg.OnAppDrawerMsg AppDrawer.Types.OnToggleOverlay)
-                    ]
-                    [ div [ X.Html.onClickStopPropagation Msg.noop ]
-                        [ div [ id "layout-main-content" ] [ appMainContent model ]
-                        ]
-                    ]
-                ]
-
-
-appMainContent model =
-    div [ id "main-view-container" ]
-        [ case Model.ViewType.getMainViewType model of
-            EntityListView viewType ->
-                Entity.View.list viewType model
-
-            SyncView ->
-                View.CustomSync.view model
-        ]
+        div [ class "mdl-typography--body-1" ] children

f87eb3b5b7a93052cbf28e1e4209a812e648bfa0
Jigar Gosar <jigar.gosar@gmail.com>
2017-07-19 12:37:05 +0530

    * try to reduce elm compile time

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -119,28 +119,6 @@ overlayViews appModel =
 
 
 
---appLayoutView m =
---    let
---        appVM =
---            ViewModel.create m
---    in
---        div [ class "x-layout" ]
---            [ div [ class "x-sidebar" ]
---                [ div [ class "x-sidebar-header" ]
---                    [ div [ class "x-sidebar-header__inner" ]
---                        [ text "foo" ]
---                    ]
---                ]
---            , div [ class "x-main" ]
---                [ div [ class "x-main-header" ]
---                    [ div [ class "x-main-header__inner" ]
---                        [ div [] [ text "a" ]
---                        ]
---                    ]
---                ]
---            ]
-
-
 appLayoutView model =
     let
         appVM =

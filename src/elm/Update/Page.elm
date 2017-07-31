4f0df2fed23d966463349a456ef000d2a30638eb
Jigar Gosar <jigar.gosar@gmail.com>
2017-07-31 17:32:01 +0530

    WIP: make-speed: page organization

--- src/elm/Update/Page.elm
+++ src/elm/Update/Page.elm
@@ -46,7 +46,7 @@ update config msg =
         PageMsg_NavigateToPath path ->
             case path of
                 "custom-sync" :: [] ->
-                    CustomSyncSettingsPage "Custom Sync" |> setPage
+                    CustomSyncSettingsPage "Advance Settings" |> setPage
 
                 _ ->
                     identity

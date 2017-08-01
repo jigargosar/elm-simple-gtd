17663efacf3c668bd191660c8e99dd7065d54817
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-01 14:02:18 +0530

    WIP: re-organizing entity-tree

---
+++ src/elm/Data/EntityTree.elm
@@ -0,0 +1,25 @@
+module Data.EntityTree exposing (..)
+
+import Entity.Types exposing (..)
+import List.Extra as List
+import Maybe.Extra as Maybe
+import Toolkit.Helpers exposing (..)
+import Toolkit.Operators exposing (..)
+import Types.GroupDoc exposing (GroupDoc)
+import Types.Todo exposing (TodoDoc)
+import X.Function exposing (..)
+import X.Function.Infix exposing (..)
+
+
+type Title
+    = GroupEntityTitle GroupEntity
+    | StringTitle String
+
+
+type Node
+    = Node Title (List TodoDoc)
+
+
+type Tree
+    = Root Node
+    | Forest List Node

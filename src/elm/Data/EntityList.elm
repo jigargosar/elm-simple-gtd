32079cd8ee37a06ac09059a7fc9d07c2a7b512cd
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-03 16:56:41 +0530

    WIP: cleanup named filters

--- src/elm/Data/EntityList.elm
+++ src/elm/Data/EntityList.elm
@@ -35,6 +35,94 @@ type alias NamedFilterModel =
     }
 
 
+namedFilterTypeList =
+    [ NF_WithNullContext
+    , NF_WithNullProject
+    , NF_FL_Done
+    , NF_FL_Recent
+    , NF_FL_Bin
+    , NF_GB_ActiveContexts
+    , NF_GB_ActiveProjects
+    , NF_WithContextId_GB_Projects
+    , NF_WithProjectId_GB_Contexts
+    ]
+
+
+type2Model namedFilterType =
+    case namedFilterType of
+        NF_WithNullContext ->
+            NamedFilterModel NF_WithNullContext
+                "Inbox"
+                IconNames.nullContext
+                Colors.nullContext
+                [ "context" ]
+                0
+
+        NF_WithNullProject ->
+            NamedFilterModel NF_WithNullProject
+                "No Project Assigned"
+                IconNames.nullProject
+                Colors.nullProject
+                [ "project" ]
+                0
+
+        NF_FL_Done ->
+            NamedFilterModel NF_FL_Done
+                "Done"
+                IconNames.done
+                Colors.sgtdBlue
+                [ "done" ]
+                0
+
+        NF_FL_Recent ->
+            NamedFilterModel NF_FL_Recent
+                "Recent"
+                IconNames.recent
+                Colors.sgtdBlue
+                [ "recent" ]
+                0
+
+        NF_FL_Bin ->
+            NamedFilterModel NF_FL_Bin
+                "Bin"
+                IconNames.bin
+                Colors.sgtdBlue
+                [ "bin" ]
+                0
+
+        NF_GB_ActiveContexts ->
+            NamedFilterModel NF_GB_ActiveProjects
+                "Projects"
+                IconNames.projects
+                Colors.projects
+                [ "projects" ]
+                0
+
+        NF_GB_ActiveProjects ->
+            NamedFilterModel NF_WithContextId_GB_Projects
+                "Context"
+                IconNames.context
+                Colors.defaultContext
+                [ "context" ]
+                1
+
+        NF_WithContextId_GB_Projects ->
+            NamedFilterModel NF_WithProjectId_GB_Contexts
+                "Project"
+                IconNames.project
+                Colors.defaultProject
+                [ "project" ]
+                1
+
+        NF_WithProjectId_GB_Contexts ->
+            NamedFilterModel NF_GB_ActiveContexts
+                "Contexts"
+                IconNames.contexts
+                Colors.contexts
+                [ "contexts" ]
+                0
+
+
 namedFilterList =
     [ NamedFilterModel NF_WithNullContext
         "Inbox"

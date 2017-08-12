561702be36c85673bec2802b8487367d7886905d
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-12 16:28:12 +0530

    WIP: cleanup update

--- src/elmx/XUpdate.elm
+++ src/elmx/XUpdate.elm
@@ -13,22 +13,21 @@ pure : model -> XReturn model msg otherMsg
 pure model =
     ( model, [], [] )
 
---map: (model -> model) -> XReturnF model msg otherMsg
---map modelF model msg otherMsg =
-
-
 
 addCmd : Cmd msg -> XReturnF model msg otherMsg
 addCmd cmd ( model, cmdList, msgList ) =
     ( model, cmd :: cmdList, msgList )
 
+
 addCmdIn : XReturn model msg otherMsg -> Cmd msg -> XReturn model msg otherMsg
-addCmdIn  =
+addCmdIn =
     flip addCmd
+
 
 addMaybeCmd : Maybe (Cmd msg) -> XReturnF model msg otherMsg
 addMaybeCmd maybeCmd return =
     maybeCmd |> Maybe.map (addCmdIn return) |> Maybe.withDefault return
+
 
 addMaybeCmdIn : XReturn model msg otherMsg -> Maybe (Cmd msg) -> XReturn model msg otherMsg
 addMaybeCmdIn =
@@ -39,19 +38,20 @@ addMsg : otherMsg -> XReturnF model msg otherMsg
 addMsg =
     List.singleton >> addMsgList
 
+
 addMsgList : List otherMsg -> XReturnF model msg otherMsg
 addMsgList otherMsgList ( model, cmdList, msgList ) =
     ( model, cmdList, msgList ++ otherMsgList )
 
 
-map modelF  ( model, cmdList, msgList ) =
+map modelF ( model, cmdList, msgList ) =
     ( modelF model, cmdList, msgList )
 
-andThen: (model -> XReturn model msg otherMsg) -> XReturnF model msg otherMsg
-andThen fn ( model, cmdList, msgList ) =
 
-        let
-            (newModel, newCmdList, newMsgList) =
-                fn model
-        in
-               (newModel, cmdList ++ newCmdList, msgList ++ newMsgList)
+andThen : (model -> XReturn model msg otherMsg) -> XReturnF model msg otherMsg
+andThen fn ( model, cmdList, msgList ) =
+    let
+        ( newModel, newCmdList, newMsgList ) =
+            fn model
+    in
+    ( newModel, cmdList ++ newCmdList, msgList ++ newMsgList )

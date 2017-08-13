ddb2e9f0c6ba48f9c1df1923cdef48596cb36948
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-13 21:34:47 +0530

    fix : frame view

--- src/elm/Views/Frame.elm
+++ src/elm/Views/Frame.elm
@@ -1,15 +1,106 @@
 module Views.Frame exposing (..)
 
+import AppDrawer.View
+import Color
+import Html exposing (text)
+import Mat
+import Material.Button
 import Material.Options exposing (..)
-import View.Layout
-import View.NewTodoFab
+import Material.Tooltip
+import View.Header
 import View.Overlays
+
+
+appLayoutView config frameVM pageContent =
+    let
+        layoutSideBarHeader =
+            AppDrawer.View.sidebarHeader frameVM
+
+        layoutSideBarContent =
+            AppDrawer.View.sidebarContent config frameVM
+
+        layoutMainHeader =
+            View.Header.appMainHeader config frameVM
+
+        onClickStopPropagationAV =
+            Mat.onClickStopPropagation config.noop
+
+        layoutMainContent =
+            div [ id "layout-main-content" ]
+                [ div [ id "page-container" ] [ pageContent ]
+                ]
+
+        layoutContent =
+            if frameVM.isSideBarOverlayOpen then
+                [ div
+                    [ id "layout-sidebar", onClickStopPropagationAV ]
+                    [ div [ cs "bottom-shadow" ] [ layoutSideBarHeader ]
+                    , layoutSideBarContent
+                    ]
+                , div
+                    [ id "layout-main"
+                    , onClick config.onToggleAppDrawerOverlay
+                    ]
+                    [ div [ onClickStopPropagationAV ]
+                        [ div [ cs "bottom-shadow" ] [ layoutMainHeader ]
+                        , layoutMainContent
+                        ]
+                    ]
+                ]
+            else
+                [ div [ cs "bottom-shadow" ]
+                    [ layoutSideBarHeader
+                    , layoutMainHeader
+                    ]
+                , div
+                    [ id "layout-sidebar", onClickStopPropagationAV ]
+                    [ layoutSideBarContent
+                    ]
+                , div
+                    [ id "layout-main"
+                    , onClick config.onToggleAppDrawerOverlay
+                    ]
+                    [ div [ onClickStopPropagationAV ] [ layoutMainContent ]
+                    ]
+                ]
+    in
+    div
+        [ id "app-layout"
+        , if frameVM.isSideBarOverlayOpen then
+            cs "sidebar-overlay"
+          else
+            nop
+        ]
+        layoutContent
+
+
+newTodoFab config m =
+    div [ cs "primary-fab-container" ]
+        [ div [ Material.Tooltip.attach config.onMdl [ 0 ] ]
+            [ Mat.fab config.onMdl
+                m.mdl
+                [ id "add-fab"
+                , Material.Button.colored
+                , Mat.onClickStopPropagation
+                    config.onStartAddingTodoWithFocusInEntityAsReference
+                , Mat.resourceId "add-todo-fab"
+                ]
+                [ Mat.iconM { name = "add", color = Color.white } ]
+            ]
+        , Material.Tooltip.render config.onMdl
+            [ 0 ]
+            m.mdl
+            [ Material.Tooltip.left ]
+            [ div [ cs "mdl-typography--body-2" ] [ text "Quick Add Task (q)" ]
+            , div [ cs "mdl-typography--body-1" ] [ text "Add To Inbox (i)" ]
+            ]
+        ]
 
 
 init frameVM =
     div [ cs "mdl-typography--body-1" ]
-        ([ View.Layout.appLayoutView frameVM.config frameVM frameVM.pageContent
-         , View.NewTodoFab.newTodoFab frameVM.config frameVM.model
+        ([ appLayoutView frameVM.config frameVM frameVM.pageContent
+         , newTodoFab frameVM.config frameVM.model
          ]
             ++ View.Overlays.overlayViews frameVM.config frameVM.model
         )
